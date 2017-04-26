a847c1158c268513b16304c7c7d294f4cf06cbce
John Crossley <jonnysnip3r@gmail.com>
2017-04-26 12:59:03 +0100

    Initial Commit

---
+++ SuperAwesomeSquares/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SuperAwesomeSquares
+//
+//  Created by John Crossley on 26/04/2017.
+//  Copyright © 2017 John Crossley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
